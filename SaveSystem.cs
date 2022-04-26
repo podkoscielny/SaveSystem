a6a5e3e31e04ed908443e4cb191deac96b82727c
Aleksander Podkościelny <podkoscielny.aleksander@gmail.com>
2022-04-27 01:46:59 +0200

    Cahnge namespace name

--- SaveSystem.cs
+++ SaveSystem.cs
@@ -5,7 +5,7 @@ using System.IO;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.Runtime.Serialization;
 
-namespace AoOkami.SaveSystem
+namespace AoOkami.SaveLoadSystem
 {
     public static class SaveSystem
     {
